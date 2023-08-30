5d57fc76a4f485062120e2e75936d7609ee7f994
System administrator <mitchcollins12@gmail.com>
2023-08-30 17:02:59 +1000

    Added Waydroid

--- configuration.nix
+++ configuration.nix
@@ -103,6 +103,7 @@
       qbittorrent
       obs-studio
       youtube-music
+      steam
     # notes
       obsidian
       zotero 
@@ -131,8 +132,10 @@
   #  virtualisation
      # pkgs.looking-glass-client
      virtmanager
+     waydroid
   ];
 
+  virtualisation.waydroid.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
