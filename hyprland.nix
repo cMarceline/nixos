86edcd535c7bfc0a072dd6c69e7ceb110a4f06ed
System administrator <mitchcollins12@gmail.com>
2023-09-04 16:09:49 +1000

    Added no apps, trying to get VFIO working

--- hyprland.nix
+++ hyprland.nix
@@ -1,7 +1,20 @@
 { config, pkgs, lib, ...}:{
 
   programs.waybar.enable = true;
-  programs.hyprland.enable = true;
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    nvidiaPatches = true;
+  };
+
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+  };
+
+  hardware = {
+    opengl.enable = true;
+    nvidia.modesetting.enable = true;
+  };
 
   environment.systemPackages = with pkgs; [
     hyprland
