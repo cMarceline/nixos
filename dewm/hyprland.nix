393d61521a8cd6b8e5c394bd6d5fd93c87003b5e
MitchCollins <mitchcollins12@gmail.com>
2023-11-09 17:40:55 +1100

    Merging Hyprland Changes

--- dewm/hyprland.nix
+++ dewm/hyprland.nix
@@ -1,36 +1,48 @@
 { config, pkgs, lib, ...}:{
-
-  programs.waybar.enable = true;
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-    nvidiaPatches = true;
+    #nvidiaPatches = true;
   };
-
+  #xdg.portal.enable = true;
+  #xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   environment.sessionVariables = {
+    # Hint electron apps to use wayland
     NIXOS_OZONE_WL = "1";
   };
-
   hardware = {
     opengl.enable = true;
-    nvidia.modesetting.enable = true;
+    #nvidia.modesetting.enable = true;
   };
+
+  imports = [
+    <home-manager/modules/services/window-managers/hyprland.nix>
+  ];
 
   environment.systemPackages = with pkgs; [
     hyprland
     kitty
-    waybar
-    eww
   ];
 
-  {pkgs, ...}: let
-  flake-compat = builtins.fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz";
-
-  hyprland = (import flake-compat {
-    src = builtins.fetchTarball "https://github.com/hyprwm/Hyprland/archive/master.tar.gz";
-  }).defaultNix;
-in {
-  imports = [
-    hyprland.homeManagerModules.default
-  ];
+  home-manager.users.marci = {
+    home.packages = [
+      pkgs.wayland
+      pkgs.hyprland
+      pkgs.wofi
+      pkgs.swww
+      pkgs.dunst
+      pkgs.kitty
+    ];
+    wayland.windowManager.hyprland.extraconfig = ''
+      
+    '';
+    
+    programs = {
+      wofi = {
+        enable = true;
+        #settings = {};
+        #style = "";
+      };
+    };
+  };
 }
